ab0eb26c8f3d880b2d0c4b82817f574ada60b85c
saumay7 <saumay7@gmail.com>
2022-11-14 23:20:48 -0800

    backend

---
+++ backend/src/data.ts
@@ -0,0 +1,77 @@
+
+
+export const sample_foods:  any[] =
+[
+  {
+    id:'1',
+    name: 'Burrito',
+    cookTime: '10-20',
+    price: 10,
+    favorite: false,
+    stars: 4.5,
+    imageUrl: 'assets/food-1.jpg',
+    tags: ['FastFood', 'Mexican', 'Lunch'],
+  },
+  {
+    id:'2',
+    name: 'Meatball',
+    price: 20,
+    cookTime: '20-30',
+    favorite: true,
+    stars: 4.7,
+    imageUrl: 'assets/food-2.jpg',
+    tags: ['SlowFood', 'Lunch'],
+  },
+  {
+    id:'3',
+    name: 'Hamburger',
+    price: 5,
+    cookTime: '10-15',
+    favorite: false,
+    stars: 3.5,
+    imageUrl: 'assets/food-3.jpg',
+    tags: ['FastFood', 'Hamburger'],
+  },
+  {
+    id:'4',
+    name: 'Fries',
+    price: 2,
+    cookTime: '15-20',
+    favorite: true,
+    stars: 3.3,
+    imageUrl: 'assets/food-4.jpg',
+    tags: ['FastFood', 'Fry'],
+  },
+  {
+    id:'5',
+    name: 'Chicken Soup',
+    price: 11,
+    cookTime: '20-50',
+    favorite: false,
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['SlowFood', 'Soup'],
+  },
+  {
+    id:'6',
+    name: 'Pizza',
+    price: 9,
+    cookTime: '40-50',
+    favorite: false,
+    stars: 4.0,
+    imageUrl: 'assets/food-6.jpg',
+    tags: ['Pizza', 'Lunch'],
+  },
+
+]
+export const sample_tags: any[] =
+[
+  { name: 'All', count: 6 },
+  { name: 'FastFood', count: 4 },
+  { name: 'Pizza', count: 2 },
+  { name: 'Lunch', count: 3 },
+  { name: 'SlowFood', count: 2 },
+  { name: 'Hamburger', count: 1 },
+  { name: 'Fry', count: 1 },
+  { name: 'Soup', count: 1 },
+]
