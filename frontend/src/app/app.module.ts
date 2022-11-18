d5627220bf3292825d12c23d46746561c1591245
saumay7 <saumay7@gmail.com>
2022-11-17 17:09:04 -0800

    Backend Url

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
-
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,6 +26,7 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { sample_foods, sample_tags } from 'src/data';
+import { FOODS_BY_ID_URL, FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL, FOODS_URL } from '../shared/constants/urls';
 import { Food } from '../shared/models/Food';
 import { Tag } from '../shared/models/Tag';
 
@@ -8,25 +11,25 @@ import { Tag } from '../shared/models/Tag';
 })
 export class FoodService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getAll(): Food[]{
-    return sample_foods;
+  getAll(): Observable<Food[]>{
+    return this.http.get<Food[]>(FOODS_URL);
   }
   getAllFoodsBySearchTerm(searchTerm: string)
   {
-    return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase())) // we want to make both the food name and searchterm lowercase
+    return this.http.get<Food[]>(FOODS_BY_SEARCH_URL + searchTerm);
 
   }
-  getFoodById(foodId : string) : Food{
-    return this.getAll().find(food => food.id == foodId) ?? new Food();
+  getFoodById(foodId : string) : Observable<Food>{
+    return this.http.get<Food>(FOODS_BY_ID_URL + foodId);
   }
-  getAllTags(): Tag[]{
-    return sample_tags;
+  getAllTags(): Observable<Tag[]>{
+    return this.http.get<Tag[]>(FOODS_BY_TAG_URL);
   }
-  getAllFoodsByTag(tag:string):Food[]{
+  getAllFoodsByTag(tag:string):Observable<Food[]>{
     return tag =="All"?
     this.getAll():
-    this.getAll().filter(food => food.tags?.includes(tag))
+    this.http.get<Food[]>(FOODS_BY_TAG_URL + tag);
   }
 }
